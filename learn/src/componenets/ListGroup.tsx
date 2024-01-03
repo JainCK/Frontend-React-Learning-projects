660483af9be3cfd5157f62c6077b3fd1a06512c7
Jain K <kuriakosejain456@gmail.com>
2024-01-03 22:21:49 +0530

    Added mousevent

--- learn/src/componenets/ListGroup.tsx
+++ learn/src/componenets/ListGroup.tsx
@@ -1,15 +1,18 @@
+import { MouseEvent } from "react";
+
 export default function ListGroup() {
   const items = ["New York", "San franciso", "Tokyo", "London", "Paris"];
 
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.map((item) => (
           <li
-            className="list-group-item"
+            className="list-group-item active"
             key={item}
-            onClick={(event) => console.log(event)}
+            onClick={handleClick}
           >
             {item}
           </li>
