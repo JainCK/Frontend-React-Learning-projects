fa3151d1acfcabdb76935e2e6ac43d3b93d65cbd
Jain K <kuriakosejain456@gmail.com>
2024-01-03 22:51:12 +0530

    Imported alert

--- learn/src/App.tsx
+++ learn/src/App.tsx
@@ -1,6 +1,7 @@
 import Message from "./Message";
 import ListGroup from "./componenets/ListGroup";
 import "./App.css";
+import Alert from "./componenets/Alert";
 
 function App() {
   const items = ["New York", "San franciso", "Tokyo", "London", "Paris"];
@@ -16,6 +17,9 @@ function App() {
         heading="Cities"
         onSelectItem={handleSelectItem}
       />
+      <Alert>
+        Hello <span> World</span>
+      </Alert>
     </div>
   );
 }
