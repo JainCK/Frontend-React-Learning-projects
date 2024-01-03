f11a21bc05a5d70a4c2dd5c347dd7447506ada5d
Jain K <kuriakosejain456@gmail.com>
2024-01-03 22:39:14 +0530

    added handleclick

--- learn/src/App.tsx
+++ learn/src/App.tsx
@@ -4,10 +4,18 @@ import "./App.css";
 
 function App() {
   const items = ["New York", "San franciso", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: String) => {
+    console.log(item);
+  };
   return (
     <div>
       <Message />
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
