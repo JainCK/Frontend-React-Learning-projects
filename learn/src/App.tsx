a00d28cdb2136a5fdfe3b1ad179e038c96f018ae
Jain K <kuriakosejain456@gmail.com>
2024-01-03 22:34:41 +0530

    added props

--- learn/src/App.tsx
+++ learn/src/App.tsx
@@ -1,15 +1,15 @@
 import Message from "./Message";
 import ListGroup from "./componenets/ListGroup";
-import './App.css'
+import "./App.css";
 
 function App() {
- return (
-  <div>
-    <Message />
-    <ListGroup />
-  </div>
-
-  )
+  const items = ["New York", "San franciso", "Tokyo", "London", "Paris"];
+  return (
+    <div>
+      <Message />
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 
-export default App
+export default App;
