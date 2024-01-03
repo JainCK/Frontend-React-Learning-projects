3831307f81a50a4ea225fbf9f217fdec27e29435
Jain K <kuriakosejain456@gmail.com>
2024-01-03 22:34:35 +0530

    Added interface

--- learn/src/componenets/ListGroup.tsx
+++ learn/src/componenets/ListGroup.tsx
@@ -1,17 +1,19 @@
 //import { MouseEvent } from "react";
+interface Props {
+  items: String[];
+  heading: String;
+}
 
 import { useState } from "react";
 
-export default function ListGroup() {
-  const items = ["New York", "San franciso", "Tokyo", "London", "Paris"];
-
+export default function ListGroup({ items, heading }: Props) {
   //let selectedIndex = 0;
   //const handleClick = (event: MouseEvent) => console.log(event);
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       <ul className="list-group">
         {items.map((item, index) => (
           <li
