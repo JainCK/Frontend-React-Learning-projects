14bd4a2f32c641c160e2fd2596cc796c6f2d4621
Jain K <kuriakosejain456@gmail.com>
2024-01-04 22:35:13 +0530

    Imported components

--- PM-Dashboard/src/App.tsx
+++ PM-Dashboard/src/App.tsx
@@ -1,10 +1,14 @@
 import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "./Components/Navigation";
+import TaskList from "./Components/TaskList";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <>
       <Navbar />
+      <TaskList />
+      <Dashboard />
     </>
   );
 }
