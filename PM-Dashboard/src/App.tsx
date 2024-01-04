699715a3f9f0c92ccb6dad8634ef6b2663a194eb
Jain K <kuriakosejain456@gmail.com>
2024-01-04 21:49:36 +0530

    Added bootstrap

---
+++ PM-Dashboard/src/App.tsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <>
+      <h3>
+        Fancy display heading
+        <small className="text-body-secondary">With faded secondary text</small>
+      </h3>
+    </>
+  );
+}
+
+export default App;
