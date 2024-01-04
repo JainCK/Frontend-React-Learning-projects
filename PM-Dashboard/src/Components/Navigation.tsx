e1f1e5dd16afa2132b176e98dab2e906e679f017
Jain K <kuriakosejain456@gmail.com>
2024-01-04 22:01:28 +0530

    Added nav componenet

---
+++ PM-Dashboard/src/Components/Navigation.tsx
@@ -0,0 +1,22 @@
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand">Project Dashbaord</a>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+}
