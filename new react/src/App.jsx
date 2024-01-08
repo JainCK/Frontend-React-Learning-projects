e4055092666c5db3cec47e45e7b52502fa1d4e7b
Jain K <kuriakosejain456@gmail.com>
2024-01-08 20:39:06 +0530

    Added tailwind styling

--- new react/src/App.jsx
+++ new react/src/App.jsx
@@ -1,15 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NavBar from './components/NavBar'
+
+import React from "react"
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <NavBar />
+     <div className="max-w-md mx-auto bg-zinc-600 shadow-md overflow-hidden rounded-md ">
+      <div className="bg-blue-400 p-4 text-white flex justify-center">
+        <h2 className="text-2xl font-semibold ">Hi, I am jain</h2>
+      </div>
+      <div className="p-4 flex justify-center">
+        <p className="text-white">
+          A full stack developer from India!!
+        </p>
+      </div>
+      <div className="p-8 flex justify-center space-x-4">
+        <a
+          href="#"
+          className="text-blue-500 hover:text-blue-700 transition duration-300"
+        >
+          <i className="fab fa-facebook-square text-5xl"></i>
+        </a>
+        <a
+          href="#"
+          className="text-blue-400 hover:text-blue-600 transition duration-300"
+        >
+          <i className="fab fa-twitter-square text-5xl"></i>
+        </a>
+        <a
+          href="#"
+          className="text-red-500 hover:text-red-700 transition duration-300"
+        >
+          <i className="fab fa-instagram-square text-5xl"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jain-c-kuriakose/
+          "
+          className="text-blue-700 hover:text-blue-900 transition duration-300"
+        >
+          <i className="fab fa-linkedin text-5xl"></i>
+        </a>
+      </div>
+    </div>
     </>
   )
 }
--- new react/src/components/NavBar.jsx
+++ new react/src/components/NavBar.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
-import { Fragment } from 'react'
 
-const NavBar = () => {
+import React from "react";
+
+const ECard = () => {
   return (
-    <Fragment>
-        <div id='conatiner' style={{
-            display: 'flex',
-            alignItems: 'center'
-        }}>
-            <h1>Logo</h1>
-            <ul > 
-                <li>About</li>
-                <li>Contact</li>
-                <li>Services</li>
-            </ul>
-        </div>
-    </Fragment>
-  )
-}
+    <div className="max-w-md mx-auto bg-white shadow-md overflow-hidden rounded-md">
+      <div className="bg-blue-500 p-4 text-white">
+        <h2 className="text-2xl font-semibold">Happy Birthday!</h2>
+      </div>
+      <div className="p-4">
+        <p className="text-gray-700">
+          Wishing you a fantastic day filled with joy and laughter. May all your
+          dreams come true!
+        </p>
+      </div>
+      <div className="p-4 flex justify-center space-x-4">
+        <a
+          href="#"
+          className="text-blue-500 hover:text-blue-700 transition duration-300"
+        >
+          <i className="fab fa-facebook-square text-3xl"></i>
+        </a>
+        <a
+          href="#"
+          className="text-blue-400 hover:text-blue-600 transition duration-300"
+        >
+          <i className="fab fa-twitter-square text-3xl"></i>
+        </a>
+        <a
+          href="#"
+          className="text-red-500 hover:text-red-700 transition duration-300"
+        >
+          <i className="fab fa-instagram-square text-3xl"></i>
+        </a>
+        <a
+          href="#"
+          className="text-blue-700 hover:text-blue-900 transition duration-300"
+        >
+          <i className="fab fa-linkedin text-3xl"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default ECard;
