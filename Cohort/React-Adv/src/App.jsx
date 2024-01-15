d0fd115419a57472b41fcefdbb63e172b3831255
Jain K <kuriakosejain456@gmail.com>
2024-01-15 19:53:57 +0530

    Added routes

--- Cohort/React-Adv/src/App.jsx
+++ Cohort/React-Adv/src/App.jsx
@@ -1,15 +1,18 @@
 import Dashboard from './Componenets/Dashboard'
 import Landing from './Componenets/Landing'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
  
 
   return (
-    <>
-    <Dashboard />
-    <Landing />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/dashboard' element={Dashboard} />
+        <Route path='/' element={Landing} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
