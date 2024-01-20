9b894ed26e06d773ac6539544b291067a63e4628
Jain K <kuriakosejain456@gmail.com>
2024-01-20 22:31:29 +0530

    Added firebase

---
+++ Cohort/tailwind/src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEKX9hHrwBT0c6V_KtGkNknoX5Tv4gdwQ",
+  authDomain: "baechat-9e8f5.firebaseapp.com",
+  databaseURL: "https://baechat-9e8f5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "baechat-9e8f5",
+  storageBucket: "baechat-9e8f5.appspot.com",
+  messagingSenderId: "682903879346",
+  appId: "1:682903879346:web:d914175dc0a87a9b1213ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
