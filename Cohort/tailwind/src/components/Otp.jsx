7d283ae0d99ce30c820f4ae70f0abd609e4957fc
Jain K <kuriakosejain456@gmail.com>
2024-01-20 22:31:37 +0530

    Otp componenet

---
+++ Cohort/tailwind/src/components/Otp.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from "react"
+import OTPInput from "otp-input-react"
+import '../App.css'
+
+const Otp = () => {
+    const [otp, setOtp] = useState()
+  return (
+    <section className='bg-black flex items-center justify-center h-screen'>
+      <div className='w-80 flex flex-col gap-4 p-4 border-2 border-solid border-white rounded'>
+        <label htmlFor="ph" className='font-bold text-2xl text-white text-center'>Enter OTP</label>
+          <OTPInput OTPLength={4} 
+          value={otp}
+          onChnage={setOtp}
+          otpType = "number"
+          disabled={false}
+          autoFocus
+          className="px-2 justify-center"
+          ></OTPInput>
+          <button className="bg-emerald-600 w-full flex gap-1 items-center justify-center py-2.5 text-white rounded">
+            <span>Verify</span>
+          </button>
+      </div>
+</section>
+  )
+}
+
+export default Otp
