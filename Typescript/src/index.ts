09aa649269a57bbd38c368b164196fa02d97e706
Jain K <kuriakosejain456@gmail.com>
2024-01-25 20:43:47 +0530

    any type

--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,4 +1,8 @@
-let age: number = 20 /* anotate age as number and csnnot reassign to string */
-if (age < 50 )
-age += 10;
-console.log(age);
+// let age: number = 20 /* anotate age as number and csnnot reassign to string */
+// if (age < 50 )
+// age += 10;
+// console.log(age);
+
+// types 
+
+let level; //any type - avoid using any type as much as possible 
