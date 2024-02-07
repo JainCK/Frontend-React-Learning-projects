54c4c47a62ec3cd2dac538c41f2a390f4c1aff60
Jain K <kuriakosejain456@gmail.com>
2024-02-07 21:49:24 +0530

    Search func done

---
+++ Typescript/ts-react/src/components/Heading.tsx
@@ -0,0 +1,11 @@
+const Heading = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-center text-xl text-slate-400">
+        Weather App
+      </h1>
+    </div>
+  );
+};
+
+export default Heading;
--- Typescript/ts-react/src/components/Search.tsx
+++ Typescript/ts-react/src/components/Search.tsx
@@ -25,9 +25,14 @@ const Search: React.FC<SearchProps> = ({ onSearch }) => {
           placeholder="Enter city "
           value={city}
           onChange={handleChange}
-          className="bg-zinc-200 text-zinc-600 font-mono ring-1 ring-zinc-400 focus:ring-2 focus:ring-rose-400 outline-none duration-300 placeholder:text-zinc-600 placeholder:opacity-50 rounded-full px-4 py-1 shadow-md focus:shadow-lg focus:shadow-rose-400"
+          className="bg-zinc-200 text-zinc-600 font-mono ring-1 ring-zinc-400 focus:ring-2 focus:ring-rose-400 outline-none duration-300 placeholder:text-zinc-600 placeholder:opacity-50 rounded-lg px-4 py-1 shadow-md focus:shadow-lg focus:shadow-rose-400"
         />
-        <button type="submit">Search</button>
+        <button
+          type="submit"
+          className="bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-4 py-1 text-base hover:border-[#fff] cursor-pointer transition ml-3"
+        >
+          Search
+        </button>
       </form>
     </>
   );
