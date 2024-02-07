1d7266b2f9c0fd29a5bffe91b99c35378ce30235
Jain K <kuriakosejain456@gmail.com>
2024-02-07 21:49:42 +0530

    project done

--- Typescript/ts-react/src/Pages/index.tsx
+++ Typescript/ts-react/src/Pages/index.tsx
@@ -1,12 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
 import Display from "../components/Display";
+import Heading from "../components/Heading";
 import Search from "../components/Search";
 
+const API_KEY = "21c36c078bcb6205808ffc8448f1a307";
+
 const Index = () => {
+  const [weather, setWeather] = useState(null);
+
+  const fetchWeather = async (city) => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      const WeatehrData = {
+        temp: response.data.main.temp,
+        desc: response.data.weather[0].description,
+      };
+      setWeather(WeatehrData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setWeather(null);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-1/4 p-6 bg-slate-500 border border-gray-200 rounded-lg ">
-        <Display />
-        <Search />
+      <div className="w-1/4 p-6 bg-stone-500  border border-gray-200 rounded-lg ">
+        <Heading />
+        <Search onSearch={fetchWeather} />
+        <Display weahter={weather} />
       </div>
     </div>
   );
