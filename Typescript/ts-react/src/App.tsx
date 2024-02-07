d3c0299cf698ba0a929e1781ddd31948a8fe89b0
Jain K <kuriakosejain456@gmail.com>
2024-02-07 19:11:02 +0530

    comp init

--- Typescript/ts-react/src/App.tsx
+++ Typescript/ts-react/src/App.tsx
@@ -1,26 +1,5 @@
-import "./App.css";
-
 function App() {
   return <></>;
 }
-
-// interface TodoType {
-//   title: string;
-//   desc: string;
-//   done: boolean;
-// }
-
-// interface TodoInput {
-//   todo: TodoType;
-// }
-
-// function Todo({ todo }: TodoInput) {
-//   return (
-//     <div>
-//       <h1>{todo.title}</h1>
-//       <h2>{todo.desc}</h2>
-//     </div>
-//   );
-// }
 
 export default App;
